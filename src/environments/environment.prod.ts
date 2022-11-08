b26c41a646c75198d392f6e5cec6e89ff8d7c24f
dArKrEsQ <razvan.bielz@gmail.com>
2022-11-08 09:22:13 +0200

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'inchiriaza-craciun',
+    appId: '1:712597046449:web:7e13ae9f6609f565d74e90',
+    storageBucket: 'inchiriaza-craciun.appspot.com',
+    apiKey: 'AIzaSyADs2Ucr3CQGjdSYB6nCR1Vzjsl6O_piWw',
+    authDomain: 'inchiriaza-craciun.firebaseapp.com',
+    messagingSenderId: '712597046449',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'inchiriaza-craciun',
+    appId: '1:712597046449:web:7e13ae9f6609f565d74e90',
+    storageBucket: 'inchiriaza-craciun.appspot.com',
+    apiKey: 'AIzaSyADs2Ucr3CQGjdSYB6nCR1Vzjsl6O_piWw',
+    authDomain: 'inchiriaza-craciun.firebaseapp.com',
+    messagingSenderId: '712597046449',
+  },
   production: false
 };
 
